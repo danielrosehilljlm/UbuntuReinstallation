c1de57b7fe1fdd406025a16e038cd1afa27e949a
danielrosehill <daniel@danielrosehill.co.il>
2020-06-14 20:40:28 +0300

    Created template for main package installer

--- Main_package_installer.sh
+++ Main_package_installer.sh
@@ -1,7 +1,24 @@
 #! /bin/bash
-
 # Installation script for Ubuntu
 
+# Change directory to the desktop
 cd ~/Desktop
-sudo apt-get update --qq
-sudo apt-get install -yy audacity gimp 
+
+# Installing PPAs
+sudo add-apt-repository ppa:webupd8team/atom
+
+
+# Update the repository
+sudo apt-get update -qq
+
+# Installing packages from main repository
+
+sudo apt-get install -y audacity gimp
+sudo apt-get install -y audacity audacity
+sudo apt-get install -y atom
+
+# Download Debian packages from the internet
+sudo wget sudo wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+
+# Install all Debian packages on the Desktop
+sudo gdebi *.deb
