9106986ff43d53d9972b075af830085a97b77c47
danielrosehill <daniel@danielrosehill.co.il>
2020-06-14 20:53:35 +0300

    Testing Git Plus package in Atom

--- Main_package_installer.sh
+++ Main_package_installer.sh
@@ -13,12 +13,11 @@ sudo apt-get update -qq
 
 # Installing packages from main repository
 
-sudo apt-get install -y audacity gimp
-sudo apt-get install -y audacity audacity
-sudo apt-get install -y atom
+sudo apt-get install -y alacarte anoise arandr audacity autokey-gtk autokey-common atom bacula bluefish blueman calibre cheese clamav clamtk clockify cloudberry-backup duplicati duplicity emacs  evince expandrive filezilla gscan2pdf gimp i3 kate keepassx keepassxc
+
 
 # Download Debian packages from the internet
 sudo wget sudo wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 
 # Install all Debian packages on the Desktop
-sudo gdebi *.deb
+sudo dpkg -i *.deb
